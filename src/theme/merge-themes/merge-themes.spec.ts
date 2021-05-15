65354aa4feaccf54543b08ce9035bcca1c909f29
Brett Dorrans <hello@lapidist.net>
2021-05-15 15:27:56 +0100

    chore: upgrade @lapidist/linting

--- src/theme/merge-themes/merge-themes.spec.ts
+++ src/theme/merge-themes/merge-themes.spec.ts
@@ -5,8 +5,7 @@ test('it merges themes', () => {
     const theme1: Theme = {
         boxShadows: {
             '0': 'none',
-            '1':
-                '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)'
+            '1': '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)'
         }
     };
 
@@ -30,8 +29,7 @@ test('it merges themes', () => {
     expect(values).toEqual({
         boxShadows: {
             '0': 'test1',
-            '1':
-                '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+            '1': '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
             '3': 'test2'
         },
         lineHeights: {
