8bd9928a9d8e83e12d1f2d5c3c2389ecc9e7b408
Brett Dorrans <hello@lapidist.net>
2022-12-06 18:39:35 +0000

    fix: optimise using chatgpt

--- src/transform.ts
+++ src/transform.ts
@@ -1,36 +1,31 @@
+import { keys } from 'object';
 import { NumberOrString, Theme, ThemeCategory } from './types';
 
-export const formatTheme = (name: string, vars: string[]): string => {
-    if (!vars.length) return '';
-    return `.theme--${name} {\n    ${vars.join('\n    ')}\n}`;
-};
+export const formatTheme = (name: string, vars: string[]): string =>
+  !vars.length ? '' : `.theme--${name} {
+    ${vars.join('\n    ')}
+  }`;
 
 export const transformCategoryToCssVars = (
-    key: string,
-    category: ThemeCategory<unknown, NumberOrString>
+  key: string,
+  { variants, values }: ThemeCategory<unknown, NumberOrString>
 ): string[] => {
-    const vars: string[] = [];
-
-    category.variants.forEach((variant, i) => {
-        const value = category.values[i];
-        if (value) {
-            vars.push(`--${key}__${variant}: ${value};`);
-        }
-    });
-
-    return vars;
+  return variants.map((variant, i) => {
+    const value = values[i];
+    return value ? `--${key}__${variant}: ${value};` : '';
+  });
 };
 
 export const transformThemeToCssVars = (name: string, theme: Theme) => {
-    let vars: string[] = [];
+  let vars: string[] = [];
 
-    Object.keys(theme).forEach(
-        (category) =>
-            (vars = [
-                ...vars,
-                ...transformCategoryToCssVars(category, theme[category])
-            ])
-    );
+  keys(theme).forEach(
+    (category) =>
+      (vars = [
+        ...vars,
+        ...transformCategoryToCssVars(category, theme[category]),
+      ])
+  );
 
-    return formatTheme(name, vars);
+  return formatTheme(name, vars);
 };
