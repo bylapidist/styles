5841f9722e650fbae2be2c38f92dce8649ffb69b
Brett Dorrans <hello@lapidist.net>
2022-12-06 18:41:19 +0000

    lint

--- src/transform.ts
+++ src/transform.ts
@@ -2,30 +2,32 @@ import { keys } from 'object';
 import { NumberOrString, Theme, ThemeCategory } from './types';
 
 export const formatTheme = (name: string, vars: string[]): string =>
-  !vars.length ? '' : `.theme--${name} {
+    !vars.length
+        ? ''
+        : `.theme--${name} {
     ${vars.join('\n    ')}
   }`;
 
 export const transformCategoryToCssVars = (
-  key: string,
-  { variants, values }: ThemeCategory<unknown, NumberOrString>
+    key: string,
+    { variants, values }: ThemeCategory<unknown, NumberOrString>
 ): string[] => {
-  return variants.map((variant, i) => {
-    const value = values[i];
-    return value ? `--${key}__${variant}: ${value};` : '';
-  });
+    return variants.map((variant, i) => {
+        const value = values[i];
+        return value ? `--${key}__${variant}: ${value};` : '';
+    });
 };
 
 export const transformThemeToCssVars = (name: string, theme: Theme) => {
-  let vars: string[] = [];
+    let vars: string[] = [];
 
-  keys(theme).forEach(
-    (category) =>
-      (vars = [
-        ...vars,
-        ...transformCategoryToCssVars(category, theme[category]),
-      ])
-  );
+    keys(theme).forEach(
+        (category) =>
+            (vars = [
+                ...vars,
+                ...transformCategoryToCssVars(category, theme[category])
+            ])
+    );
 
-  return formatTheme(name, vars);
+    return formatTheme(name, vars);
 };
